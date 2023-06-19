1056f5f4f19de9300c34c621fe818e3ab4dab7b5
j3rryl <jeremymunroe0@gmail.com>
2023-06-19 18:13:12 +0300

    bottom navigation

--- app/src/main/java/com/example/irense/MainActivity.kt
+++ app/src/main/java/com/example/irense/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.irense
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<MeowBottomNavigation>(R.id.btm_nav)
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.person))
+        bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.bar_chart))
+        bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.list))
+
     }
 }
